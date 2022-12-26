e133ee84fe7bc968651339e65710b284fb15ad5f
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-25 23:16:36 -0300

    Utilizando o bloco using para encerrar as operações após a leitura do arquivo

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,25 +6,29 @@ class Program
     static void Main(string[] args)
     {
         string enderecoDoArquivo = "C:\\Developer\\courses\\Alura\\ByteBankIO-master\\contas.txt";
-        int numeroDeBytesLidos = -1;
-        
-        
-        FileStream fluxoDoARquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        // public override int Read(byte[] array, int offset, int cout); -- Read do FileStream
-        byte[] buffer = new byte[1024]; // 1Kb
-
-        while (numeroDeBytesLidos != 0)
+        using (FileStream fluxoDoARquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoARquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+            int numeroDeBytesLidos = -1;
+        
+        
+        
+            // public override int Read(byte[] array, int offset, int cout); -- Read do FileStream
+            byte[] buffer = new byte[1024]; // 1Kb
 
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoARquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+
+            }
+        
+            // Devoluções:
+            // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+            // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+            // zero, se o final do fluxo for atingido
+        
+            fluxoDoARquivo.Close();
         }
-        
-        // Devoluções:
-        // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
-        // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
-        // zero, se o final do fluxo for atingido
-        
         
         Console.ReadLine();
     }
